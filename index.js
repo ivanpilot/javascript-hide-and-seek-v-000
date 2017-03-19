4ca39a5dfbd1f5f14ae4719cc3f57bc8e8bc0a1d
Ivan Pilot <ivanpilot@gmail.com>
2017-03-19 21:57:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  var rankedList = document.querySelectorAll('.ranked-list')
+
+  for(var i = 0, l = rankedList.length; i < l; i++){
+    var children = rankedList[i].children
+
+    for (var j = 0, k = children.length; j < k; j++){
+      children[j].innerHTML = (parseInt(children[j].innerHTML,10) + n).toString()
+    }
+  }
+}
+
+function deepestChild(){
+  var element = document.getElementById('grand-node');
+  var currentDiv;
+  var nextDiv = element;
+
+  while(nextDiv !== null){
+    currentDiv = nextDiv
+    nextDiv = currentDiv.querySelector('div')
+  }
+
+  return currentDiv //.innerHTML
+}
